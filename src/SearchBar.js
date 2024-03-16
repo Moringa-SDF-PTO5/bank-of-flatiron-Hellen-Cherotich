19ca08e2fdc987eb499ac0ec4aab018cc0a6fa7a
cherotich <cherohellen14@gmail.com>
2024-03-16 13:31:51 -0700

    royal

---
+++ src/SearchBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SearchBar = ({onSearch}) => {
+    const handleChange = e => {
+        const searchTerm = e.target.value;
+        onSearch(searchTerm)
+    };
+
+return (
+    <input
+    type="text"
+    placeholder="Search by description"
+    onChange={handleChange}
+    />
+);
+};
