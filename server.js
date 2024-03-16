7dada7afc19df6f97583c25b5b7f994da3a62d63
cherotich <cherohellen14@gmail.com>
2024-03-16 14:34:15 -0700

    royal

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-// server.js
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
  
-server.use(middlewares)
-server.use('', router)
+server.use(middlewares);
+server.use(router); // Use the router for all routes
 server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
-})
+  console.log('JSON Server is running');
+});
