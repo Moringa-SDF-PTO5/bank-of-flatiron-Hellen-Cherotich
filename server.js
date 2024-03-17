f8b3db432f26b5cd1e31fedb9e8986c90af933bb
cherotich <cherohellen14@gmail.com>
2024-03-17 07:39:33 -0700

    change final

--- server.js
+++ server.js
@@ -2,10 +2,16 @@
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
- 
+
 server.use(middlewares);
-server.use(router); // Use the router for all routes
-server.listen(process.env.PORT || 3000, () => {
-  console.log('JSON Server is running');
+server.use(router);
+
+// Console log to verify db.json content
+console.log('Content of db.json:');
+console.log(require('./db.json'));
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
 
