34d9eb421ed03783146f436d7ed6bf967f5642c2
cherotich <cherohellen14@gmail.com>
2024-03-22 15:11:43 -0700

    change

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
+// index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = document.getElementById('root');
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
 reportWebVitals();
-
-export default indexedDB.jimport React from "react";
-import { ReactDOM } from "react-dom/client";
-import App from "../App";
-
-const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(<App/>)
