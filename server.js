344be7ff1d2b3dd2e915a663beb26a063f7f66c2
cherotich <cherohellen14@gmail.com>
2024-03-22 16:18:20 -0700

    changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 // Define route handler for the root URL
 app.get('/', (req, res) => {
   // Send the main HTML file or render your application's main page here
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
