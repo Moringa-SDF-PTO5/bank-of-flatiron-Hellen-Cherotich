3c3ba3fdb64fa4fcea67a9a3821540c0f034b51b
cherotich <cherohellen14@gmail.com>
2024-03-22 05:38:52 -0700

    bottl

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const path = require('path');
+import express, { json, static } from 'express';
+import { join } from 'path';
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware for parsing JSON bodies
-app.use(express.json());
+app.use(json());
 
 // Serve static files from the build directory
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(static(join(__dirname, 'build')));
 
 // Serve index.html for all routes
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(join(__dirname, 'build', 'index.html'));
 });
 
 // Example POST route - replace with your own route handlers
