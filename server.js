75f85e5fbba5a5ff9d30dea2f003d868ad714363
cherotich <cherohellen14@gmail.com>
2024-03-23 06:00:16 -0700

    new

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
 // Serve static files from the 'build' directory
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Serve static files from the 'src' directory
-app.use(express.static(path.join(__dirname, 'src')));
+// Define the API proxy
+app.use(
+  '/api', // Replace '/api' with the path you want to proxy
+  createProxyMiddleware({
+    target: 'http://json-server-2bly.onrender.com', // Target the root URL of the JSON server
+    changeOrigin: true,
+  })
+);
 
 // Define route handler for the root URL
 app.get('/', (req, res) => {
   // Send the main HTML file or render your application's main page here
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
